ac68bf0866ca66cc9ddbe6c6f1c18c73122d7f1f
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-23 14:46:53 +0200

    [SocketHandler.java]Aggiornati import

--- SocketHandler.java
+++ SocketHandler.java
@@ -5,7 +5,6 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
