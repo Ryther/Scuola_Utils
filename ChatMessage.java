dddb7f15902c3c66d118ad346d0e84fe33d02a9a
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-24 17:30:43 +0200

    [ChatMessage.java]Aggiunto serialVersion

--- ChatMessage.java
+++ ChatMessage.java
@@ -12,7 +12,8 @@ import java.util.logging.Logger;
  */
 public class ChatMessage implements Serializable {
     
-    private String username;
+	private static final long serialVersionUID = 1L;
+	private String username;
     private LocalDateTime dateTime;
     private String message;
 
